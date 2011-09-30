0c3f31d5735e74dc80037fdf8289cc6e81b76b7d
Filippo Spiga <spiga.filippo@gmail.com>
2011-09-30 14:07:23 +0000

    Improvements...
    git-svn-id: http://qeforge.qe-forge.org/svn/phigemm/trunk/phiGEMM@69 0d18c639-378f-4888-944e-ee8602ca9468

--- testing/serial/compute_split_matrix.f90
+++ testing/serial/compute_split_matrix.f90
@@ -7,12 +7,13 @@ PROGRAM compute_split_matrix
     DOUBLE PRECISION, ALLOCATABLE   ::  A(:,:), B(:,:), C(:,:)
     DOUBLE PRECISION                :: alpha, beta
     INTEGER               :: m, n, k, lda, ldb, ldc
+    CHARACTER*1           :: transa, transb
 
     INTEGER     :: ierr, counter
 
     DOUBLE PRECISION    :: time_start, time_stop
     DOUBLE PRECISION    :: times(1:3)
-    DOUBLE PRECISION    :: collects(1:100)
+    DOUBLE PRECISION    :: collects(1:1000,1:2)
 
     REAL    :: mysplit(1:4)
     REAL    :: x
@@ -21,12 +22,21 @@ PROGRAM compute_split_matrix
     EXTERNAL                    :: cublas_DGEMM
     DOUBLE PRECISION, EXTERNAL  :: cclock
 
-    m = 1000
-    n = 1000
-    k = 1000
-    lda = m
+    m = 2206
+    k = 146
+    n = 3972
+    transa = 'T'
+    transb = 'N'
+
+!        int lda = m;
+!        int ldb = k;
+!        if ( is_transa[ count ] ) lda = k;
+!        if ( is_transb[ count ] ) ldb = n;
+
+    lda = k
     ldb = k
     ldc = m
+
     alpha = 1.0D0
     beta = 1.0D0
     ALLOCATE( A(m,k), B(k,n), C(m,n) )
@@ -37,13 +47,13 @@ PROGRAM compute_split_matrix
 
 !    ierr = cublas_init()
     time_start = cclock()
-    CALL cublas_DGEMM ('N','N', m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+    CALL cublas_DGEMM (transa,transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
     time_stop = cclock()
 !    ierr = sublas_shutdown()
     times(1) = time_stop-time_start
 
     time_start = cclock()
-    CALL DGEMM ('N','N', m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+    CALL DGEMM (transa,transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
     time_stop = cclock()
     times(2) = time_stop-time_start
 
@@ -52,7 +62,9 @@ PROGRAM compute_split_matrix
 #endif
 
     counter = 1
-    collects(:) = 1000
+    collects(:,1) = 1000
+    collects(:,2) = 0
+
     DO x = 0.4, 0.975, 0.025
 
         mysplit(:) = x
@@ -62,9 +74,12 @@ PROGRAM compute_split_matrix
 #endif
 
         time_start = cclock()
-        CALL phiDgemm ('N','N', m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
+        CALL phiDgemm (transa,transb, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc)
         time_stop = cclock()
-        collects(counter) = time_stop-time_start
+        collects(counter,1) = time_stop-time_start
+        collects(counter,2) = x
+
+        WRITE (*, '("time=",F8.6," split=",F8.6)') collects(counter,1) , collects(counter,2)
 
         counter = counter + 1
     END DO
@@ -73,7 +88,7 @@ PROGRAM compute_split_matrix
     CALL CloseCudaEnv()
 #endif
 
-    WRITE (*, '(/5x,"CUBLAS=",F8.6," BLAS=",F8.6," PHIGEMM=",F8.6)') times(1), times(2), MINVAL(collects)
+    WRITE (*, '(/5x,"CUBLAS=",F8.6," BLAS=",F8.6," PHIGEMM=",F8.6,"(split=",F8.6,")")') times(1), times(2), MINVAL(collects(:,1)) , collects(MINLOC(collects(:,1)),2)
 
     DEALLOCATE( A, B, C)
 
