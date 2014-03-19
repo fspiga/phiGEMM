49e7e511e34cd1b6428f14d04a9bd6d3d5179075
Filippo Spiga <spiga.filippo@gmail.com>
2014-03-19 10:36:19 +0000

    call-by-call profiler for CPU-only codes. It is just the beginning...

---
+++ src/cpu-profiler.c
@@ -0,0 +1,10 @@
+/*****************************************************************************\
+ * Copyright (C) 2011-2014 Quantum ESPRESSO Foundation
+ *
+ * This file is distributed under the terms of the
+ * GNU General Public License. See the file `License'
+ * in the root directory of the present distribution,
+ * or http://www.gnu.org/copyleft/gpl.txt .
+ *
+ * Filippo Spiga (filippo.spiga@quantum-espresso.org)
+\*****************************************************************************/
