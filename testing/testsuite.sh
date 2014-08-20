ff5d4fbbb30bb72a512701c45acfa2bdd6aa076e
Filippo Spiga <spiga.filippo@gmail.com>
2014-08-20 11:38:38 +0100

    Adjusted test case, added TransA and TransB params

--- testing/testsuite.sh
+++ testing/testsuite.sh
@@ -2,12 +2,16 @@
 
 NUMA_CTL="numactl -m 1 -c 1"
 
+sleep 1
+
+export CUDA_VISIBLE_DEVICES=0
+
 echo "'\nTesting DGEMM\n"
 make TEST_DATATYPE_FLAGS=-D__CUDA_TYPE_DOUBLE
-env LD_LIBRARY_PATH=../lib:${LD_LIBRARY_PATH} ${NUMA_CTL} ../bin/single_test.x 1 4096 4096 4096 0.6 0.91 0.1
+env LD_LIBRARY_PATH=../lib:${LD_LIBRARY_PATH} ${NUMA_CTL} ../bin/single_test-dgemm.x 1 N N 4096 4096 4096 0.95 0.995 0.01
 
 sleep 1
 
 echo "'\nTesting ZGEMM\n"
 make TEST_DATATYPE_FLAGS=-D__CUDA_TYPE_DOUBLE_COMPLEX
-env LD_LIBRARY_PATH=../lib:${LD_LIBRARY_PATH} ${NUMA_CTL} ../bin/single_test.x 1 4096 4096 4096 0.6 0.91 0.1
+env LD_LIBRARY_PATH=../lib:${LD_LIBRARY_PATH} ${NUMA_CTL} ../bin/single_test-zgemm.x 1 N N 4096 4096 4096 0.95 0.995 0.01
